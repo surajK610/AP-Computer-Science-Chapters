d37b175051eec96e295d687c51269d3703eb3e09
surajK610 <suraj@Surajs-MBP.lan>
2017-04-17 19:18:48 -0700

    Strings

---
+++ Chapter 7/strings.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class strings here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class strings
+{
+    public static void main (String [] args)
+    {
+        String a = "12";
+        System.out.println(a.substring(1, 2));
+    }
+}
